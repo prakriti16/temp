dc003159ff929735ed240598728da756b138ae7c
prakriti16 <70594062+prakriti16@users.noreply.github.com>
2024-10-02 21:03:42 +0530

    ././

--- app.py
+++ app.py
@@ -125,4 +125,6 @@ def load_chat_history():
     return []
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 4000))
+    # Run the app
+    app.run(host='0.0.0.0', port=port)
